793728eae1753305301d07c3ee5c5266f6e51b65
Stafford Williams <stafford.williams@gmail.com>
2021-09-21 22:32:15 +1000

    running function

--- lib/headlines-cdk-stack.ts
+++ lib/headlines-cdk-stack.ts
@@ -1,9 +1,20 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as lambda from '@aws-cdk/aws-lambda';
+import {NodejsFunction} from '@aws-cdk/aws-lambda-nodejs';
+import * as path from 'path';
 
 export class HeadlinesCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new s3.Bucket(this, 'headlines-cdk-bucket');
+    new NodejsFunction(this, 'headlines-cdk-parseFeed', {
+      // memorySize: 1024,
+      // timeout: cdk.Duration.seconds(5),
+      // runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'handle',
+      entry: path.join(__dirname, `/../src/parseFeed.ts`),
+    });
   }
 }
---
+++ src/parseFeed.ts
@@ -0,0 +1,31 @@
+import { ScheduledHandler } from "aws-lambda";
+import Parser from "rss-parser";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+
+export const handle: ScheduledHandler = async () => {
+  const feed = await parseFeed();
+  //await persist(feed);
+};
+
+const parseFeed = async () => {
+  const feed = await new Parser().parseURL(
+    "http://feeds.bbci.co.uk/news/world/rss.xml"
+  );
+  console.log(`Retrieved ${feed.items.length} items from ${feed.title}`);
+  return feed;
+};
+
+const persist = async (body: any) => {
+  const date = new Date();
+  const key = `${date.getFullYear()}/${
+    date.getMonth() + 1
+  }/${date.getDay()}/${date.getHours()}${date.getMinutes()}.json`;
+  await new S3Client({ region: process.env.AWS_REGION }).send(
+    new PutObjectCommand({
+      Bucket: process.env.BUCKET_NAME,
+      Key: key,
+      Body: JSON.stringify(body, null, 2),
+    })
+  );
+  console.log(`Saved ${key}`);
+};
